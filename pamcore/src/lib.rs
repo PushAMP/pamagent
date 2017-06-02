7dd2cdc9252c3a687345b24e7b4a460396fc7264
Dmitriy Zhiltsov <dmitriy@pushamp.com>
2017-06-02 13:04:33 +0300

    Refactor

--- pamcore/src/lib.rs
+++ pamcore/src/lib.rs
@@ -10,10 +10,9 @@ extern crate serde_derive;
 extern crate lazy_static;
 extern crate rand;
 use cpython::{PyResult, Python};
-use std::sync::Mutex;
+use std::sync::RwLock;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
-use std::cell::RefCell;
 
 #[derive(Debug, Serialize)]
 struct StackNode {
@@ -55,8 +54,8 @@ impl StackNode {
 #[derive(Debug, Serialize)]
 struct TransactionNode {
     base_name: String,
-    nodes_stack: RefCell<Vec<StackNode>>,
-    trace_node_count: RefCell<u8>,
+    nodes_stack: Vec<StackNode>,
+    trace_node_count: u8,
     guid: String,
     path: String,
 }
@@ -66,8 +65,8 @@ impl TransactionNode {
     }
 }
 lazy_static! {
-    static ref TRANSACTION_CACHE: Mutex<HashMap<u64, TransactionNode>> = {
-        let m = Mutex::new(HashMap::new());
+    static ref TRANSACTION_CACHE: RwLock<HashMap<u64, TransactionNode>> = {
+        let m = RwLock::new(HashMap::new());
         m
     };
 }
@@ -109,14 +108,14 @@ fn set_transaction(_: Python,
                    transaction: String,
                    path: Option<String>)
                    -> PyResult<bool> {
-    let mut tr_cache = TRANSACTION_CACHE.lock().unwrap();
+    let mut tr_cache = TRANSACTION_CACHE.write().unwrap();
     match tr_cache.entry(id) {
         Entry::Occupied(_) => Ok(false),
         Entry::Vacant(v) => {
             v.insert(TransactionNode {
                          base_name: transaction,
-                         nodes_stack: RefCell::new(vec![]),
-                         trace_node_count: RefCell::new(0),
+                         nodes_stack: vec![],
+                         trace_node_count: 0,
                          guid: format!("{:x}", rand::random::<u64>()),
                          path: path.unwrap_or(format!("")),
                      });
@@ -126,7 +125,7 @@ fn set_transaction(_: Python,
 }
 
 fn set_transaction_path(_: Python, id: u64, path: String) -> PyResult<bool> {
-    let mut tr_cache = TRANSACTION_CACHE.lock().unwrap();
+    let mut tr_cache = TRANSACTION_CACHE.write().unwrap();
     match tr_cache.get_mut(&id) {
         Some(tr) => tr.set_path(path),
         None => return Ok(false),
@@ -137,7 +136,7 @@ fn set_transaction_path(_: Python, id: u64, path: String) -> PyResult<bool> {
 
 
 fn get_transaction(_: Python, id: u64) -> PyResult<Option<u64>> {
-    let tr_cache = TRANSACTION_CACHE.lock().unwrap();
+    let tr_cache = TRANSACTION_CACHE.read().unwrap();
     match tr_cache.get(&id) {
         Some(_) => Ok(Some(id)),
         None => Ok(None),
@@ -145,14 +144,14 @@ fn get_transaction(_: Python, id: u64) -> PyResult<Option<u64>> {
 }
 
 fn get_transaction_start_time(_: Python, id: u64) -> PyResult<f64> {
-    let tr_cache = TRANSACTION_CACHE.lock().unwrap();
+    let tr_cache = TRANSACTION_CACHE.read().unwrap();
     let tr = match tr_cache.get(&id) {
         Some(tr) => tr,
         None => return Ok(0.0),
     };
     let st: f64;
-    if tr.nodes_stack.borrow().len() > 0 {
-        st = tr.nodes_stack.borrow()[0].start_time;
+    if tr.nodes_stack.len() > 0 {
+        st = tr.nodes_stack[0].start_time;
     } else {
         st = 0.0
     };
@@ -160,14 +159,14 @@ fn get_transaction_start_time(_: Python, id: u64) -> PyResult<f64> {
 }
 
 fn get_transaction_end_time(_: Python, id: u64) -> PyResult<f64> {
-    let tr_cache = TRANSACTION_CACHE.lock().unwrap();
+    let tr_cache = TRANSACTION_CACHE.read().unwrap();
     let tr = match tr_cache.get(&id) {
         Some(tr) => tr,
         None => return Ok(0.0),
     };
     let st: f64;
-    if tr.nodes_stack.borrow().len() > 0 {
-        st = tr.nodes_stack.borrow()[0].end_time;
+    if tr.nodes_stack.len() > 0 {
+        st = tr.nodes_stack[0].end_time;
     } else {
         st = 0.0
     };
@@ -175,51 +174,51 @@ fn get_transaction_end_time(_: Python, id: u64) -> PyResult<f64> {
 }
 
 fn push_current(_: Python, id: u64, node_id: u64, start_time: f64) -> PyResult<bool> {
-    let tr_cache = TRANSACTION_CACHE.lock().unwrap();
-    let c_tr = match tr_cache.get(&id) {
+    let mut tr_cache = TRANSACTION_CACHE.write().unwrap();
+    let c_tr = match tr_cache.get_mut(&id) {
         Some(v) => v,
         None => return Ok(false),
     };
-    c_tr.nodes_stack.borrow_mut().push(StackNode {
-                                           node_id: node_id,
-                                           childrens: vec![],
-                                           start_time: start_time,
-                                           end_time: 0.0,
-                                           exclusive: 0.0,
-                                           node_count: 0,
-                                           duration: 0.0,
-                                       });
+    c_tr.nodes_stack.push(StackNode {
+                              node_id: node_id,
+                              childrens: vec![],
+                              start_time: start_time,
+                              end_time: 0.0,
+                              exclusive: 0.0,
+                              node_count: 0,
+                              duration: 0.0,
+                          });
     return Ok(true);
 }
 
 fn pop_current(_: Python, id: u64, node_id: u64, end_time: f64) -> PyResult<Option<u64>> {
-    let tr_cache = TRANSACTION_CACHE.lock().unwrap();
-    let c_tr = match tr_cache.get(&id) {
+    let mut tr_cache = TRANSACTION_CACHE.write().unwrap();
+    let c_tr = match tr_cache.get_mut(&id) {
         Some(v) => v,
         None => return Ok(None),
     };
-    let ln = c_tr.nodes_stack.borrow().len();
+    let ln = c_tr.nodes_stack.len();
     if ln == 1 {
-        let ref mut root_id = c_tr.nodes_stack.borrow_mut()[0];
+        let ref mut root_id = c_tr.nodes_stack[0];
         root_id.set_endtime(end_time);
         root_id.comp_exclusive();
-        *c_tr.trace_node_count.borrow_mut() += 1;
+        c_tr.trace_node_count += 1;
         return Ok(None);
     };
-    let cur_id = match c_tr.nodes_stack.borrow_mut().pop() {
+    let cur_id = match c_tr.nodes_stack.pop() {
         Some(mut v) => {
             v.set_endtime(end_time);
             v.comp_exclusive();
-            *c_tr.trace_node_count.borrow_mut() += 1;
+            c_tr.trace_node_count += 1;
             v
         }
         None => return Ok(None),
     };
-    let ln = c_tr.nodes_stack.borrow().len();
+    let ln = c_tr.nodes_stack.len();
     println!("LLLLL {:?}", ln);
 
     if cur_id.node_id == node_id {
-        let ref mut parent_node = c_tr.nodes_stack.borrow_mut()[ln - 1];
+        let ref mut parent_node = c_tr.nodes_stack[ln - 1];
         parent_node.process_child(cur_id);
         let t = parent_node.node_id;
         println!("PARENT {}", t);
@@ -231,7 +230,7 @@ fn pop_current(_: Python, id: u64, node_id: u64, end_time: f64) -> PyResult<Opti
 }
 
 fn drop_transaction(_: Python, id: u64) -> PyResult<bool> {
-    let mut tr_cache = TRANSACTION_CACHE.lock().unwrap();
+    let mut tr_cache = TRANSACTION_CACHE.write().unwrap();
     match tr_cache.remove(&id) {
         Some(val) => {
             let j = serde_json::to_string(&val).unwrap_or("".to_uppercase());
