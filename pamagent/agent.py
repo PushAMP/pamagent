71546b4ba5c190bc5414dc8c91abbf55ab078018
Dmitriy Zhiltsov <dmitriy@pushamp.com>
2017-09-29 14:37:37 +0300

    Resolve issue #PAMP-5 Stage Review

--- pamagent/agent.py
+++ pamagent/agent.py
@@ -1,10 +1,12 @@
 from pamagent.hooks import requests_hook, django_hook
+from pamagent import pamagent_core
 
 
-def _init_builtin():
+def _init_builtin() -> None:
         requests_hook.patch()
         django_hook.patch()
 
 
-def init():
+def init(collector_host: str="pamcollector.pushamp.com")->None:
     _init_builtin()
+    pamagent_core.activate(collector_host)
